9257288438ddb6b9967dfcba9764fb520702d6f5
sugiyasu kei <igetiwant@gmail.com>
2015-05-16 02:08:23 +0900

    勉強中

--- main.js
+++ main.js
@@ -1,2 +1,31 @@
-var element = document.querySelector("#greeting");
-element.innerText = "雛形";
+var Contract = function(){
+  this.no = "default no";
+  this.cur = "default cur";
+  this.mount = "default mount";
+  this.rate = "default rate";
+};
+
+var Data = function(){
+  this.name = "default name";
+  this.cur = "default cur";
+  this.mount = "default mount";
+  this.contracts = [new Contract()];
+};
+
+var module = angular.module('hinagataApp', []);
+module.service('datas', ['$rootScope', function ($scope) {
+  var datas = [new Data()];
+  
+  $scope.$watch(function () {
+    return datas;
+  }, function (value) {
+    alert(JSON.stringify(value));
+    $scope.$broadcast('change:datas', value);
+  }, true);
+  
+  this.datas = datas;
+}]);
+
+module.controller('HinagataController', ['$scope', 'datas', function ($scope, datas) {
+  angular.extend($scope, datas.datas[0]);
+}]);
